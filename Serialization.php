72000929d00ed3accb40a707e38066841f1a31cc
jeroendedauw <jeroendedauw@gmail.com>
2013-07-09 19:49:01 +0200

    Added entry point file

---
+++ Serialization.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This documentation group collects source code files belonging to the Serialization library.
+ *
+ * @defgroup Serialization Serialization
+ */
+
+if ( defined( 'Serialization_VERSION' ) ) {
+	// Do not initialize more then once.
+	return;
+}
+
+define( 'Serialization_VERSION', '1.0 alpha' );
+
+// @codeCoverageIgnoreStart
+spl_autoload_register( function ( $className ) {
+	$className = ltrim( $className, '\\' );
+	$fileName = '';
+	$namespace = '';
+
+	if ( $lastNsPos = strripos( $className, '\\') ) {
+		$namespace = substr( $className, 0, $lastNsPos );
+		$className = substr( $className, $lastNsPos + 1 );
+		$fileName  = str_replace( '\\', '/', $namespace ) . '/';
+	}
+
+	$fileName .= str_replace( '_', '/', $className ) . '.php';
+
+	$namespaceSegments = explode( '\\', $namespace );
+
+	if ( $namespaceSegments[0] === 'Serializer' || $namespaceSegments[0] === 'Deserializer' ) {
+		if ( count( $namespaceSegments ) === 1 || $namespaceSegments[1] !== 'Tests' ) {
+			require_once __DIR__ . '/src/' . $fileName;
+		}
+	}
+} );
+// @codeCoverageIgnoreEnd
