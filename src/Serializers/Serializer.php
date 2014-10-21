2539db07e4403a271efa05dbc0852de14346462c
Jeroen De Dauw <jeroendedauw@gmail.com>
2014-10-21 14:53:01 +0200

    Update Serializer.php

--- src/Serializers/Serializer.php
+++ src/Serializers/Serializer.php
@@ -13,6 +13,17 @@ use Serializers\Exceptions\SerializationException;
 interface Serializer {
 
 	/**
+	 * Returns the provided object as a representation suitable for serialization via
+	 * for instance json_encode.
+	 * 
+	 * Serializers normally support only one or a few types
+	 * of objects, since they do the type specific part of the wider serialization
+	 * process. An exception must be thrown when an object that is not supported by
+	 * the serializer is passed to it.
+	 * 
+	 * Serializers should provide a transformation of the whole object, without adding
+	 * any additional data fetched via services.
+	 * 
 	 * @since 1.0
 	 *
 	 * @param mixed $object
