0ed873f03534271a8763cb8398f4e1bafdd7c6dd
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-05-11 17:07:59 +0200

    Drop dead code from MissingTypeException

--- src/Deserializers/Exceptions/MissingTypeException.php
+++ src/Deserializers/Exceptions/MissingTypeException.php
@@ -12,14 +12,4 @@ namespace Deserializers\Exceptions;
  */
 class MissingTypeException extends DeserializationException {
 
-	protected $unsupportedType;
-
-	/**
-	 * @param string $message
-	 * @param \Exception $previous
-	 */
-	public function __construct( $message = '', \Exception $previous = null ) {
-		parent::__construct( $message, $previous );
-	}
-
 }
